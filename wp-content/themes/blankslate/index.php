9bd4bcbb8afe2880a115af6e10d9aa49a964a990
JoSch <johannes@stahl-r.de>
2019-07-24 07:04:21 +0200

    removed static dummy content from homepage markup

--- wp-content/themes/blankslate/index.php
+++ wp-content/themes/blankslate/index.php
@@ -18,67 +18,6 @@
           meinem Gegenüber abhängt.
         </p>
       </div>
-
-      <div class="content-item post text--def">
-        <h2>Nominierungen für den 55. Grimme-Preis</h2>
-        <p class="content">
-          Freiheit, Anerkennung, Liebe, ein selbstbestimmtes
-          Leben: Drei Schwestern begegnen
-          sich im Taumel verschiedener Sehnsüchte.
-          Aber ihre gegenseitige Abhängigkeit hält sie gefangen und konfrontiert
-          sie mit ihrem Gegenüber und sich Selbst. Was bedeutet
-          es gebraucht zu werden in einer Weise, die keine Kompromisse
-          zulässt. Oder umgekehrt, wenn das eigene Leben von
-          meinem Gegenüber abhängt.
-        </p>
-      </div>
-
-      <div class="content-item post text--def">
-        <h2>Nominierungen für den 55. Grimme-Preis</h2>
-        <p class="content">
-          Freiheit, Anerkennung, Liebe, ein selbstbestimmtes
-          Leben: Drei Schwestern begegnen
-          sich im Taumel verschiedener Sehnsüchte.
-          Aber ihre gegenseitige Abhängigkeit hält sie gefangen und konfrontiert
-          sie mit ihrem Gegenüber und sich Selbst. Was bedeutet
-          es gebraucht zu werden in einer Weise, die keine Kompromisse
-          zulässt. Oder umgekehrt, wenn das eigene Leben von
-          meinem Gegenüber abhängt.
-        </p>
-      </div>
-
-      <div class="content-item text text--def">
-        <p class="content">
-          Die Schauspielagentur RZ A wurde im Jahr 1996 in Hamburg gegründet.
-          Geschäftsführerinnen sind Marie-Luise Schmidt und
-          Beate Wolgast. Der Firmensitz wurde zum
-          1.1. 2000 nach Berlin verlegt.
-        </p>
-
-        <p class="content">
-          Von Anfang an war das Konzept der Agentur darauf ausgerichtet,
-          eine überschaubare Anzahl von Künstlerinnen und Künstlern
-          zu vertreten, um eine intensive und persönliche Betreuung
-          zu gewährleisten. Es wird großen Wert auf die Individualität der
-          einzelnen Klienten gelegt. Unverwechselbare Gesichter bei den
-          Schauspielerinnen und Schauspielern sowie eine eigene Handschrift
-          bei den Regisseuren und Kameramännern, so könnte ein
-          Motto der Agentur lauten.
-        </p>
-        
-        <p class="content">
-          Beate Wolgast arbeitete nach ihrer Ausbildung zur Werbekauffrau
-          und des Studiums der Kommunikationswissenschaft zwei
-          Jahre als Werbetexterin, anschließend als Redakteurin und Textchefin
-          bei Premiere für die Sendung „0137“ und das Kinomagazin.
-          Danach war sie ein Jahr für eine TV-Produktionsfirma in Berlin
-          als Redaktionsleiterin und Producerin für ein wöchentliches
-          Magazin verantwortlich.
-        </p>  
-      </div>
-
-      <div class="content-item info"></div>
-      
     </section>
 
     <section id="content_right" class="w--50"></section>
