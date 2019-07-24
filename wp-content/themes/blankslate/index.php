56e12ecd52bea8419f6cc2959ef5f38c8e31b3bd
JoSch <johannes@stahl-r.de>
2019-07-24 07:21:25 +0200

    construction of basic loop to fetch content from CMS

--- wp-content/themes/blankslate/index.php
+++ wp-content/themes/blankslate/index.php
@@ -5,19 +5,13 @@
 
     <section id="content_left" class="w--50 px--2 pt--10">
       <!-- Content Items -->
-      <div class="content-item post text--def">
-        <h2>Nominierungen für den 55. Grimme-Preis</h2>
-        <p class="content">
-          Freiheit, Anerkennung, Liebe, ein selbstbestimmtes
-          Leben: Drei Schwestern begegnen
-          sich im Taumel verschiedener Sehnsüchte.
-          Aber ihre gegenseitige Abhängigkeit hält sie gefangen und konfrontiert
-          sie mit ihrem Gegenüber und sich Selbst. Was bedeutet
-          es gebraucht zu werden in einer Weise, die keine Kompromisse
-          zulässt. Oder umgekehrt, wenn das eigene Leben von
-          meinem Gegenüber abhängt.
-        </p>
-      </div>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div id="post-<?php the_ID(); ?>" class="<?php foreach((get_the_category()) as $category){echo $category->name;} ?> content-item text--def">
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+        </div>
+      <?php endwhile; endif; ?>
+
     </section>
 
     <section id="content_right" class="w--50"></section>
