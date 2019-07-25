683c5ec88df17cd72d52440dd5c9eb5af0388d41
JoSch <johannes@stahl-r.de>
2019-07-25 11:54:51 +0200

    added dynamic fetching of thumbnail-url

--- wp-content/themes/blankslate/content-actor.php
+++ wp-content/themes/blankslate/content-actor.php
@@ -1,6 +1,6 @@
 <div class="grid-item" id="<?php echo sanitize_title_with_dashes(get_the_title()) ?>">
   <figure > 
-    <img src="https://placeimg.com/480/680/any">
+    <img src="<?php the_post_thumbnail_url(); ?>">
     <figcaption><?php the_title(); ?></figcaption>
   </figure>
 </div>
