ad7280b4faaceed855909f9784400913cd5a8687
JoSch <johannes@stahl-r.de>
2019-08-07 06:27:32 +0200

    setup of CSS necessary to float img on right side of article's text-block

--- wp-content/themes/blankslate/content.php
+++ wp-content/themes/blankslate/content.php
@@ -1,10 +1,8 @@
 <article id="post-<?php the_ID(); ?>" class="content-item text--def">
 
-  <header>
-    <h2><?php the_title(); ?></h2>
-  </header>
-
   <div>
+    <img src="https://www.fillmurray.com/g/140/100">
+    <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
   </div>
   
