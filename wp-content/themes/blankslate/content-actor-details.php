ce0466b26845274313e5dc15b63febcecf2e8913
JoSch <johannes@stahl-r.de>
2019-08-10 15:03:31 +0200

    added new field to actor-details CMS and markup (»Weitere Produktionen«) + styled last item in actor-chronologie

--- wp-content/themes/blankslate/content-actor-details.php
+++ wp-content/themes/blankslate/content-actor-details.php
@@ -26,8 +26,11 @@
     </div>
 
     <?php if( have_rows('lebenslauf') ): ?>
+      <?php $all_items_count = count(get_field('lebenslauf'));
+            $item_count = 1;
+            ?>
       <?php while ( have_rows('lebenslauf') ) : the_row(); ?>
-        <section class="mb--1">
+        <section class="mb--1 <?php if ($item_count == $all_items_count): ?>mb--2 <?php endif ?>">
           <h2><?php the_sub_field('jahr'); ?></h2>
 
           <?php if( have_rows('produktionen') ): ?>
@@ -35,6 +38,28 @@
               <div class="text--il">
                 <p class="h2"><?php the_sub_field('titel'); ?></p>
                 <span>Regie: <?php the_sub_field('regie'); ?></span>
+              </div>
+            <?php endwhile ?>
+          <?php endif ?>
+
+        </section> 
+        <?php $item_count++; ?>
+
+      <?php endwhile ?>
+    <?php endif ?>
+
+    <h2 class="mb--1">Weitere Produktionen</h2>
+
+    <?php if( have_rows('lebenslauf_weitere') ): ?>
+      <?php while ( have_rows('lebenslauf_weitere') ) : the_row(); ?>
+        <section class="mb--1">
+          <h2><?php the_sub_field('jahr'); ?></h2>
+
+          <?php if( have_rows('produktionen') ): ?>
+            <?php while ( have_rows('produktionen') ) : the_row(); ?>
+              <div class="text--il">
+                <p class="h2"><?php the_sub_field('titel'); ?></p>
+                <span><?php the_sub_field('regie'); ?></span>
               </div>
             <?php endwhile ?>
           <?php endif ?>
--- wp-content/themes/blankslate/single.php
+++ wp-content/themes/blankslate/single.php
@@ -39,8 +39,11 @@
           ?>
           
           <?php if( have_rows('lebenslauf') ): ?>
+            <?php $all_items_count = count(get_field('lebenslauf'));
+                  $item_count = 1;
+                  ?>
             <?php while ( have_rows('lebenslauf') ) : the_row(); ?>
-              <section class="mb--1">
+              <section class="mb--1 <?php if ($item_count == $all_items_count): ?>mb--2 <?php endif ?>">
                 <h2><?php the_sub_field('jahr'); ?></h2>
 
                 <?php if( have_rows('produktionen') ): ?>
@@ -48,6 +51,28 @@
                     <div class="text--il">
                       <p class="h2"><?php the_sub_field('titel'); ?></p>
                       <span>Regie: <?php the_sub_field('regie'); ?></span>
+                    </div>
+                  <?php endwhile ?>
+                <?php endif ?>
+
+              </section> 
+              <?php $item_count++; ?>
+
+            <?php endwhile ?>
+          <?php endif ?>
+
+          <h2 class="mb--1">Weitere Produktionen</h2>
+
+          <?php if( have_rows('lebenslauf_weitere') ): ?>
+            <?php while ( have_rows('lebenslauf_weitere') ) : the_row(); ?>
+              <section class="mb--1">
+                <h2><?php the_sub_field('jahr'); ?></h2>
+
+                <?php if( have_rows('produktionen') ): ?>
+                  <?php while ( have_rows('produktionen') ) : the_row(); ?>
+                    <div class="text--il">
+                      <p class="h2"><?php the_sub_field('titel'); ?></p>
+                      <span><?php the_sub_field('regie'); ?></span>
                     </div>
                   <?php endwhile ?>
                 <?php endif ?>
