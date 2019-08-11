99d6800c7260022986be66822602120d13479b1c
JoSch <johannes@stahl-r.de>
2019-08-11 07:35:29 +0200

    nav-menu provided as template-part

---
+++ wp-content/themes/blankslate/templates/snippets/menu.php
@@ -0,0 +1,52 @@
+<header id="header" class="z--high mb--2">
+  <button id="button_nav" class="text text--lg" onclick="toggle_menu()">
+    +
+  </button>
+  
+  <div id="nav_main" class="d--flex text--lg text--b w--100">
+
+    <div class="f1">
+      <div class="d--flex v">
+        <?php 
+          $menu = wp_get_nav_menu_object('Hauptmenü');  
+          $menu_items = wp_get_nav_menu_items($menu->term_id);
+          //
+          foreach ( (array) $menu_items as $key => $menu_item ) {
+            $title = $menu_item->title;
+            $url = $menu_item->url;
+            $cat = $menu_item->object_id;
+            $menu_link = '<a class="align-left actor-link" data-content-cat="' . $cat . '">' . $title . '</a>';
+            //
+            echo $menu_link;
+          }
+        ?>
+      </div>
+
+      <div class="d--flex h">
+        <?php 
+          $menu = wp_get_nav_menu_object('Untermenü');  
+          $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+          foreach ( (array) $menu_items as $key => $menu_item ) {
+            $title = $menu_item->title;
+            $url = $menu_item->url;
+            $cat = $menu_item->object_id;
+            //
+            if($title == 'Agentur' || $title == 'Kontakt') {
+              $menu_link = '<span class="list menu-link content-link" data-content-cat="' . $title . '" ><a>' . $title . '</a></span>';
+              echo $menu_link;
+            } else {
+              $menu_link = '<span class="list menu-link content-link" data-content-cat="' . $cat . '" ><a data-namespace="' . $title . '">' . $title . '</a></span>';
+              echo $menu_link;
+            }
+          }
+        ?>
+      </div>
+    </div>
+
+    <div class="f1 d--flex h right align-top">
+      <a id="brand_link" href="<?php echo get_home_url(); ?>">RZ A</a>
+    </div>
+    
+  </div>  
+</header>
