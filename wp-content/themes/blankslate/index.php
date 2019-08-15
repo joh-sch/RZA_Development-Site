2082d9fab48efa47eee72b9a03bf575c40f8062c
JoSch <johannes@stahl-r.de>
2019-08-15 08:26:00 +0200

    extension of mobile-page-build logic -> adding mobile-grid-classes to left column

--- wp-content/themes/blankslate/index.php
+++ wp-content/themes/blankslate/index.php
@@ -5,8 +5,8 @@
     <?php get_template_part( 'templates/snippets/menu'); ?>
     
     <!-- Content Sections -->
-    <section id="content_left" data-namespace="News" class="w--50 noActor">
-      <section id="content_left_items">
+    <section id="content_left" data-namespace="News" class="w--50 noActor <?php if (wp_is_mobile()): ?> grid<?php endif ?>">
+      <section id="content_left_items" class="<?php if (wp_is_mobile()): ?> grid-container mobile<?php endif ?>">
         <?php
           if ( wp_is_mobile() ) {
             $args = array('cat' => array(7,8));
