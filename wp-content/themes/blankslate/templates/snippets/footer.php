e2c0db53ab3d5566eea71fdd8515732057018035
JoSch <johannes@stahl-r.de>
2019-09-16 06:58:15 +0200

    added text-file to store build-version + function to read & display info in console

--- wp-content/themes/blankslate/templates/snippets/footer.php
+++ wp-content/themes/blankslate/templates/snippets/footer.php
@@ -13,6 +13,20 @@
       resize_zigzag();
       init_cookieConsent();
     });
+
+    function meta_info(file){
+      var rawFile = new XMLHttpRequest();
+      rawFile.open("GET", file, false);
+      rawFile.onreadystatechange = function () {
+          if(rawFile.readyState === 4) {
+              if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                console.log(allText);
+              }
+          }
+      }
+      rawFile.send(null);
+    }
   </script>
 </body>
 </html>
