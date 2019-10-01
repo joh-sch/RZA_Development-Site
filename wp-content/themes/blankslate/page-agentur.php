44ca80e246870f72d06a02edaeda38118bf2e39b
JoSch <johannes@stahl-r.de>
2019-10-01 09:43:09 +0200

    revised/cleaned up mobile arch./markup of »Agentur« page

--- wp-content/themes/blankslate/page-agentur.php
+++ wp-content/themes/blankslate/page-agentur.php
@@ -2,40 +2,52 @@
 
   <main id="content" class="d--flex">
     
-    <!-- Nav-Menu -->
-    <?php get_template_part( 'templates/snippets/menu'); ?>
+    <!-- Navigation Menu -->
+    <?php if(wp_is_mobile()) {
+      get_template_part( 'templates/snippets/menu_mobile');
+    } else {
+      get_template_part( 'templates/snippets/menu');
+    } ?>
     
-    <!-- Content Sections -->
-    <section id="content_left" data-namespace="<?php echo get_the_title(); ?>" class="w--50 noActor <?php if (wp_is_mobile()): ?> grid<?php endif ?>">
-      <section id="content_left_items" class="<?php if (wp_is_mobile()): ?> grid-container mobile<?php endif ?>">
-        
-        <!-- Load »Agentur« content -->
-        <?php 
-          $page = get_page_by_title('Agentur');
-          $content = '<div class="content-item text--def">' . $page->post_content . '</div>';
-          echo $content
-        ?>
-
+    <!-- Site Content -->
+    <?php if(wp_is_mobile()): ?>
+      <section id="content_mobile" data-namespace="<?php echo get_the_title(); ?>" class="text--def bg--white default-container mobile">
+        <!-- »Kontakt« content -->
+        <?php $page = get_page_by_title('Agentur');
+              $content = '<div class="content-item text--def">' . $page->post_content . '</div>';
+              echo $content ?>
       </section>
-    </section>
+    <?php else: ?>
+      <!-- Content Sections -->
+      <section id="content_left" data-namespace="<?php echo get_the_title(); ?>" class="w--50 noActor <?php if (wp_is_mobile()): ?> grid<?php endif ?>">
+        <section id="content_left_items" class="<?php if (wp_is_mobile()): ?> grid-container mobile<?php endif ?>">
+          
+          <!-- Load »Agentur« content -->
+          <?php 
+            $page = get_page_by_title('Agentur');
+            $content = '<div class="content-item text--def">' . $page->post_content . '</div>';
+            echo $content
+          ?>
 
-    <!-- Zig-Zag -->
-    <?php get_template_part( 'templates/snippets/zigzag'); ?>
+        </section>
+      </section>
+      <!-- Zig-Zag -->
+      <?php get_template_part( 'templates/snippets/zigzag'); ?>
+      <section id="content_right" class="w--50 text--def">
+        <div class="grid-container">
 
-    <section id="content_right" class="w--50 text--def">
-      <div class="grid-container">
+          <?php
+            $args = array('cat' => array(7,8));
+            $loop = new WP_Query($args); 
+          ?>
 
-        <?php
-          $args = array('cat' => array(7,8));
-          $loop = new WP_Query($args); 
-        ?>
-
-        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-          <?php get_template_part( 'content-actor' ); ?>
-        <?php endwhile; ?>
-        
-      </div>
-    </section>
+          <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+            <?php get_template_part( 'content-actor' ); ?>
+          <?php endwhile; ?>
+          
+        </div>
+      </section>
+    <?php endif ?>
 
   </main>
 
--- wp-content/themes/blankslate/page-kontakt.php
+++ wp-content/themes/blankslate/page-kontakt.php
@@ -12,35 +12,33 @@
     <!-- Site Content -->
     <?php if(wp_is_mobile()): ?>
       <section id="content_mobile" data-namespace="<?php echo get_the_title(); ?>" class="text--def bg--white default-container mobile">
-          
-          <!-- »Kontakt« content -->
-          <?php $page = get_page_by_title('Kontakt');
-                $content = '<div class="content-item text--def">' . $page->post_content . '</div>';
-                echo $content ?>
+        <!-- »Kontakt« content -->
+        <?php $page = get_page_by_title('Kontakt');
+              $content = '<div class="content-item text--def">' . $page->post_content . '</div>';
+              echo $content ?>
 
-          <!-- Social Media -->
-          <div class="content-item text--lg">
-            <?php if(have_rows("social_media")): ?>
-              <?php while ( have_rows('social_media') ) : the_row(); ?>
-                <a href="<?php the_sub_field("instagram") ?>" target="_blank"><i class="fab fa-instagram mr--1"></i></a>
-                <a href="<?php the_sub_field("facebook") ?>" target="_blank"><i class="fab fa-facebook-f mr--1"></i></a>
-                <a href="<?php the_sub_field("twitter") ?>" target="_blank"><i class="fab fa-twitter"></i></a>
-              <?php endwhile ?>
-            <?php endif ?>
+        <!-- Social Media -->
+        <div class="content-item text--lg">
+          <?php if(have_rows("social_media")): ?>
+            <?php while ( have_rows('social_media') ) : the_row(); ?>
+              <a href="<?php the_sub_field("instagram") ?>" target="_blank"><i class="fab fa-instagram mr--1"></i></a>
+              <a href="<?php the_sub_field("facebook") ?>" target="_blank"><i class="fab fa-facebook-f mr--1"></i></a>
+              <a href="<?php the_sub_field("twitter") ?>" target="_blank"><i class="fab fa-twitter"></i></a>
+            <?php endwhile ?>
+          <?php endif ?>
+        </div>
+
+        <!-- Imprint & PP -->
+        <div class="content-item">
+          <button class="collapsible--button text text--lg text--b">Impressum</button>
+          <div class="collapsible--content text--def mb--1-plus">
+            <?php the_field('impressum') ?>
           </div>
-
-          <!-- Imprint & PP -->
-          <div class="content-item">
-            <button class="collapsible--button text text--lg text--b">Impressum</button>
-            <div class="collapsible--content text--def mb--1-plus">
-              <?php the_field('impressum') ?>
-            </div>
-            <button class="collapsible--button text text--lg text--b">Datenschutz</button>
-            <div class="collapsible--content text--def">
-              <?php the_field('datenschutz') ?>
-            </div>
+          <button class="collapsible--button text text--lg text--b">Datenschutz</button>
+          <div class="collapsible--content text--def">
+            <?php the_field('datenschutz') ?>
           </div>
-
+        </div>
       </section>
     <?php else: ?>
       <!-- Content Sections -->
@@ -77,10 +75,8 @@
 
         </section>
       </section>
-
       <!-- Zig-Zag -->
       <?php get_template_part('templates/snippets/zigzag'); ?>
-
       <section id="content_right" class="w--50 text--def">
         <div class="grid-container">
 
